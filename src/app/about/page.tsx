66098bfecebbd5e9fef3493da1175755780a25bb
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-18 22:57:51 +0300

    server component comment

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,6 @@
 export default function AboutPage() {
+  console.log("About server component");
   return <h1>About Page</h1>;
 }
+
+// Cant put state in server component. Need client component for that.
