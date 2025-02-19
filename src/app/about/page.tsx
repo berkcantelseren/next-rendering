1dfcdd31a0bb2c27f0ec4aefac7c346d74c13910
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-19 23:19:10 +0300

    about page lorem add for test

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,11 @@
 export default function AboutPage() {
   console.log("About server component");
-  return <h1>About Page</h1>;
+  return (
+    <div>
+      <h1>About Page</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit.</p>
+    </div>
+  );
 }
 
 // Cant put state in server component. Need client component for that.
