c0c953fd8a6e4b5a1548e3d7da026e9367629169
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-19 23:20:44 +0300

    add longer text for visibility

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,8 +3,11 @@ export default function AboutPage() {
   return (
     <div>
       <h1>About Page</h1>
-      <p className="text-2xl text-white bg-red-200 text-nowrap mt-12">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+      <p className="text-2xl text-black bg-red-200 text-nowrap mt-12">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut delectus
+        blanditiis nesciunt, quo ad quam praesentium officia, sint laudantium
+        quas amet consectetur tempore veniam quibusdam commodi placeat quidem
+        illo autem?
       </p>
     </div>
   );
