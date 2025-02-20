cf6956bb6a2ec01767ab2e0497c688f4d015be54
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-20 22:43:51 +0300

    about page time add

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,13 +2,7 @@ export default function AboutPage() {
   console.log("About server component");
   return (
     <div>
-      <h1>About Page</h1>
-      <p className="text-2xl text-black bg-red-200 text-nowrap mt-12">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut delectus
-        blanditiis nesciunt, quo ad quam praesentium officia, sint laudantium
-        quas amet consectetur tempore veniam quibusdam commodi placeat quidem
-        illo autem?
-      </p>
+      <h1>About Page {new Date().toLocaleTimeString()}</h1>
     </div>
   );
 }
