b552d86859574eccec10d0cc4bf597373443f081
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-21 21:20:08 +0300

    add serverSideFunction for testing

---
+++ src/utils/server-utils.ts
@@ -0,0 +1,9 @@
+export const serverSideFunction = () => {
+  console.log(
+    `use multiple libraries,
+      use env. variables,
+      interact with a database
+    `
+  );
+  return "server result";
+};
