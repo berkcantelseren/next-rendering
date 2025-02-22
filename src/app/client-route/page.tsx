7f0285fba18bf168b1283fc4a37b358d12525cf2
Berkcan Telseren <berkcantelseren@gmail.com>
2025-02-22 22:53:45 +0300

    use clientSideFunction in client-route

--- src/app/client-route/page.tsx
+++ src/app/client-route/page.tsx
@@ -2,12 +2,15 @@
 
 import React from "react";
 import { useTheme } from "../components/theme-provider";
+import { clientSideFunction } from "@/utils/client-utils";
 
 export default function ClientRoutePage() {
   const theme = useTheme();
+  const result = clientSideFunction();
   return (
     <div style={{ color: theme.colors.secondary }}>
       <h1>Client route page</h1>
+      <p>{result}</p>
     </div>
   );
 }
